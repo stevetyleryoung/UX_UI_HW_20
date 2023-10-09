46bcf2be41990eee8e3625af33e0cc05872ce7b9
stevetyleryoung <131972544+stevetyleryoung@users.noreply.github.com>
2023-10-10 10:56:02 +1100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,47 @@
+console.log("Yourindex.js file is loaded correctly!!")
+
+
+$(".get-in-touch-button").hover(
+  function() {
+    $(this).css({
+      "text-decoration": "underline",
+      "color": "#EFCF48"
+    });
+  },
+  function() {
+    $(this).css({
+      "text-decoration": "none",
+      "color": "white"
+    });
+  }
+);
+
+$(".case-study-button").hover(
+  function() {
+    $(this).css({
+      "text-decoration": "underline",
+      "color": "#EFCF48"
+    });
+  },
+  function() {
+    $(this).css({
+      "text-decoration": "none",
+      "color": "white"
+    });
+  }
+);
+
+$(".get-in-touch-footer").hover(
+  function() {
+    $(this).css({
+      "text-decoration": "underline",
+      "color": "#EFCF48"
+    });
+  },
+  function() {
+    $(this).css({
+      "text-decoration": "none",
+      "color": "white"
+    });
+  }
+);
